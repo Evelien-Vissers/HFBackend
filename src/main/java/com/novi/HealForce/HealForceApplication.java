09f49d3fe8ea0578a7f5c9cbe1ac3223c4cbf346
Evelien-Vissers <vissersevelien@gmail.com>
2024-09-02 14:46:16 +0200

    Eindproject Initialisatie

---
+++ src/main/java/com/novi/HealForce/HealForceApplication.java
@@ -0,0 +1,13 @@
+package com.novi.HealForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novi/HealForce/HealForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.HealForce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
