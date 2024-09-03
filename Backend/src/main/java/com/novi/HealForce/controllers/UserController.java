9fb4f620e2034b214a498045eadac1d10c7295bb
Evelien-Vissers <vissersevelien@gmail.com>
2024-09-03 21:40:06 +0200

    UserController done

--- Backend/src/main/java/com/novi/HealForce/controllers/UserController.java
+++ Backend/src/main/java/com/novi/HealForce/controllers/UserController.java
@@ -1,4 +1,44 @@
 package com.novi.HealForce.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    // POST /users - Registreer een nieuwe gebruiker
+    @PostMapping
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = userService.registerUser(userDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    // GET /users/{userId} - Haal informatie op van een specifieke gebruiker
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) {
+        UserDTO userDTO = userService.getUserById(userId);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    // PUT /users/{userId} - Werk informatie van een specifieke gebruiker bij
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO) {
+        UserDTO updatedUser = userService.updateUser(userId, userDTO);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    // DELETE /users/{userId} - Verwijder een specifieke gebruiker
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
